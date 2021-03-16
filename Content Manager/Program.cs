9628dbba757dcdae90f3182bb5b95d4a6849414b
David Spörri <davepermen@hotmail.com>
2021-03-16 23:54:10 +0100

    reading them obj files

--- Content Manager/Program.cs
+++ Content Manager/Program.cs
@@ -11,8 +11,13 @@ namespace Content_Manager
     {
         static async Task Main(string[] args)
         {
-            var source = Directory.From(@"C:\Users\spoda\source\repos\far-off-wanderer\game") / "Content" / "src";
-            var destiny = Directory.From(@"C:\Users\spoda\source\repos\far-off-wanderer\game") / "Content" / "bin";
+            var directory = Directory.Common.Current;
+            while (directory.Filtered("*.sln", allDirectories: false).Any() == false)
+            {
+                directory = directory.Parent;
+            }
+            var source = directory / "Content" / "src";
+            var destiny = directory / "Content" / "bin";
 
             var vessels = source / "vessels";
 
@@ -27,79 +32,61 @@ namespace Content_Manager
                 Console.WriteLine($"file: {item.Name}");
                 Console.WriteLine($"name: {item.Content.Name}");
 
-                var lines = await (item.Parent / Filename.FromExtended(item.Content.Mesh)).ReadLines();
+                var vertices = await (item.Parent / Filename.FromExtended(item.Content.Mesh)).ReadFromWavefrontObjFormat();
+                await (destiny / vessels.Name / Filename.From(item.Content.Name, "mesh")).WriteAsMesh(vertices);
 
-                var positions = lines.Where(l => l.StartsWith("v ")).Select(line =>
-                {
-                    var s = line.Split(' ').Skip(1).Select(s => float.Parse(s)).ToArray();
-                    return new Vector3(s[0], s[1], s[2]);
-                }).ToArray();
-
-                var normals = lines.Where(l => l.StartsWith("vn ")).Select(line =>
-                {
-                    var s = line.Split(' ').Skip(1).Select(s => float.Parse(s)).ToArray();
-                    return new Vector3(s[0], s[1], s[2]);
-                }).ToArray();
-
-                var textureCoordinates = lines.Where(l => l.StartsWith("vt ")).Select(line =>
-                {
-                    var s = line.Split(' ').Skip(1).Select(s => float.Parse(s)).ToArray();
-                    return new Vector2(s[0], s[1]);
-                }).ToArray();
-
-                var faces = lines.Where(l => l.StartsWith("f ")).Select(line =>
-                {
-                    var s = line.Split(' ').Skip(1).ToArray();
-
-                    var a = s[0].Split('/').Select(s => int.Parse(s) - 1).ToArray();
-                    var b = s[1].Split('/').Select(s => int.Parse(s) - 1).ToArray();
-                    var c = s[2].Split('/').Select(s => int.Parse(s) - 1).ToArray();
-
-                    return new Face
-                    {
-                        A = new Vertex
-                        {
-                            Position = positions[a[0]],
-                            TextureCoordinate = textureCoordinates[a[1]],
-                            Normal = normals[a[2]]
-                        },
-                        B = new Vertex
-                        {
-                            Position = positions[b[0]],
-                            TextureCoordinate = textureCoordinates[b[1]],
-                            Normal = normals[b[2]]
-                        },
-                        C = new Vertex
-                        {
-                            Position = positions[c[0]],
-                            TextureCoordinate = textureCoordinates[c[1]],
-                            Normal = normals[c[2]]
-                        },
-                    };
-                }).ToArray();
-
-                Console.WriteLine($"\tvertices:  {positions.Length}");
-                Console.WriteLine($"\tnormals:   {normals.Length}");
-                Console.WriteLine($"\ttexcoords: {textureCoordinates.Length}");
-                Console.WriteLine($"\tfaces:     {faces.Length}");
+                Console.WriteLine($"\tfaces: {vertices.Length / 3f}");
 
                 Console.WriteLine();
             }
         }
     }
 
-    record Vertex
+    record Vertex(Vector3 Position, Vector2 TextureCoordinate, Vector3 Normal);
+
+    static class FiletypeObjReader
     {
-        public Vector3 Position;
-        public Vector3 Normal;
-        public Vector2 TextureCoordinate;
+        public static async Task<Vertex[]> ReadFromWavefrontObjFormat(this File file)
+        {
+            var lines = await file.ReadLines();
+
+            var positions = lines.Where(l => l.StartsWith("v ")).Select(line =>
+            {
+                var s = line.Split(' ').Skip(1).Select(s => float.Parse(s)).ToArray();
+                return new Vector3(s[0], s[1], s[2]);
+            }).ToArray();
+
+            var normals = lines.Where(l => l.StartsWith("vn ")).Select(line =>
+            {
+                var s = line.Split(' ').Skip(1).Select(s => float.Parse(s)).ToArray();
+                return new Vector3(s[0], s[1], s[2]);
+            }).ToArray();
+
+            var textureCoordinates = lines.Where(l => l.StartsWith("vt ")).Select(line =>
+            {
+                var s = line.Split(' ').Skip(1).Select(s => float.Parse(s)).ToArray();
+                return new Vector2(s[0], s[1]);
+            }).ToArray();
+
+            return lines.Where(l => l.StartsWith("f ")).SelectMany(line =>
+            {
+                var s = line.Split(' ').Skip(1).ToArray();
+                var all = s.Select(s => s.Split('/').Select(s => int.Parse(s) - 1).ToArray()).ToArray();
+                return all.Select(v => new Vertex(positions[v[0]], textureCoordinates[v[1]], normals[v[2]])).ToArray();
+            }).ToArray();
+        }
     }
 
-    record Face
+    static class FiletypeMeshWriter
     {
-        public Vertex A;
-        public Vertex B;
-        public Vertex C;
+        public static async Task WriteAsMesh(this File file, Vertex[] vertices)
+        {
+            byte[] GetBytesV3(Vector3 v) => BitConverter.GetBytes(v.X).Concat(BitConverter.GetBytes(v.Y)).Concat(BitConverter.GetBytes(v.Z)).ToArray();
+            byte[] GetBytesV2(Vector2 v) => BitConverter.GetBytes(v.X).Concat(BitConverter.GetBytes(v.Y)).ToArray();
+            byte[] GetBytes(Vertex v) => GetBytesV3(v.Position).Concat(GetBytesV3(v.Normal)).Concat(GetBytesV2(v.TextureCoordinate)).ToArray();
+
+            await file.WriteBytes(vertices.SelectMany(GetBytes).ToArray());
+        }
     }
 
     namespace ContentTypes
